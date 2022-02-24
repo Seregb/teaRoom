62aa9f30f168ee9e6764ce18b8b53c0b4c863550
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-02-24 12:56:11 +0300

    fixes

--- app.js
+++ app.js
@@ -9,11 +9,10 @@ const indexRouter = require('./routes/index.js');
 const teaRouter = require('./routes/tea.js');
 const userRouter = require('./routes/user.js');
 const adminRouter = require('./routes/admin');
-const {addLocals} = require('./middleware/allmidleware')
+const { addLocals } = require('./middleware/allmidleware');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-
 
 const app = express();
 
@@ -27,42 +26,45 @@ app.use(express.static(path.join(process.env.PWD, 'public')));
 hbs.registerPartials(path.join('views', 'partials'));
 app.use('/tea', teaRouter);
 
-app.use(session({
-  secret: 'apojrglkdzfng;sakg', // строка для шифрования сессии
-  resave: false, // не пересохраняем сессию если не было изменений
-  saveUninitialized: false, // не сохраняем сессию если она пустая
-  cookie: { secure: false }, // не HTTPS
-  name: 'userCookie', // имя сессионной куки
-  store: new FileStore(), // хранилище для куков - папка с файлами
-}));
+app.use(
+  session({
+    secret: 'apojrglkdzfng;sakg', // строка для шифрования сессии
+    resave: false, // не пересохраняем сессию если не было изменений
+    saveUninitialized: false, // не сохраняем сессию если она пустая
+    cookie: { secure: false }, // не HTTPS
+    name: 'userCookie', // имя сессионной куки
+    store: new FileStore(), // хранилище для куков - папка с файлами
+  })
+);
 
-app.use(addLocals)
+app.use(addLocals);
 
 app.use('/users', userRouter);
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
 app.use(cookieParser());
 
+// app.use((req, res, next) => {
+//   const error = createError(
+//     404,
+//     'Запрашиваемой страницы не существует на сервере.'
+//   );
+//   next(error);
+// });
 
-
-app.use((req, res, next) => {
-  const error = createError(404, 'Запрашиваемой страницы не существует на сервере.');
-  next(error);
-});
-
-app.use(function (err, req, res, next) {
-  const appMode = req.app.get('env');
-  let error;
-  if (appMode === 'development') {
-    error = err;
-  } else {
-    error = {};
-  }
-  res.locals.message = err.message;
-  res.locals.error = error;
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use((err, req, res, next) => {
+//   const appMode = req.app.get('env');
+//   let error;
+//   if (appMode === 'development') {
+//     error = err;
+//   } else {
+//     error = {};
+//   }
+//   res.locals.message = err.message;
+//   res.locals.error = error;
+//   res.status(err.status || 500);
+//   // res.render('error');
+// });
 
 const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => {
