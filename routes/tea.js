fad1563f2ca0af73233975ab0a615f8a8bf9a3c3
Seregb <seregb68@gmail.com>
2022-02-24 18:26:33 +0300

    Tea page: order Comment & time catch

--- routes/tea.js
+++ routes/tea.js
@@ -14,19 +14,24 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+function timer(timeCom) {
+  return new Date() - timeCom;
+}
+
 router
   .route('/:id')
   .get(async (req, res) => {
     const teaId = req.params.id;
-    // console.log(upload);
     try {
-      const {
-        id, name, description, img, place,
-      } = await Tea.findOne({ where: { id: teaId } });
-      const comment = await Comment.findAll({ where: { id: teaId } });
+      const { id, name, description, img, place } = await Tea.findOne({ where: { id: teaId } });
+
+      const comment = await Comment.findAll({ where: { tea_id: teaId }, order: [['updatedAt', 'DESC']] });
       const user = await User.findOne({ where: { id: comment[0].user_id } });
+      const time = Math.floor(timer(comment[0].updatedAt) / 100000);
+      const timeAnswer = time === 0 ? `было только что` : `${time} мин. назад`;
+      
       return res.render('tea', {
-        id, name, description, img,
+        comment, id, name, description, img, timeAnswer, time,
       });
     } catch (err) {
       // console.log(err);
@@ -35,7 +40,7 @@ router
   .post(async (req, res) => {
     const user = await User.findOne({ where: { id: 1 } });
     const userComm = await Comment.create({ user_id: 1, tea_id: 1, text: req.body.comment });
-    res.sendStatus(200);
+    res.json(userComm);
   });
 
 module.exports = router;
