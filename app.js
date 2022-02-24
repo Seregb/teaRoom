7031a5e58d3ceb0b711fe98477906603725a1a16
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-02-24 12:31:03 +0300

    added admin page+form

--- app.js
+++ app.js
@@ -7,16 +7,19 @@ const FileStore = require('session-file-store')(session);
 require('dotenv').config();
 const indexRouter = require('./routes/index.js');
 const userRouter = require('./routes/user.js');
+const adminRouter = require('./routes/admin');
 const app = express();
 
 app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(process.env.PWD, 'public')));
 hbs.registerPartials(path.join('views', 'partials'));
 
 app.use('/users', userRouter);
 app.use('/', indexRouter);
+app.use('/admin', adminRouter);
 
 const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => {
